c25ef42ec5bac2c7d9d0f9126425b8b7a9a037f0
Marcin Lewandowski <marcin@ais.pl>
2014-05-22 07:35:54 +0200

    psake

--- S3_build/upload.ps1
+++ S3_build/upload.ps1
@@ -2,6 +2,13 @@ $base_dir  = resolve-path .
 $version = "3.0.0-SNAPSHOT"
 $uploader = "..\Uploader\S3Uploader.exe"
 
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath
+
+get-module psake | remove-module
+
+import-module (Get-ChildItem "$scriptDir\packages\psake.*\tools\psake.psm1" | Select-Object -First 1)
+
 Write-Host "Starting upload"
 if (Test-Path $uploader) {
 	$log = "RavenDB Java Client v3.0"
@@ -10,7 +17,7 @@ if (Test-Path $uploader) {
 	
 	Write-Host "Executing: $uploader ""$currentUploadCategory"" ""$version"" $file ""$log"""
 	
-	start-process $uploader "$currentUploadCategory" "$version" $file "$log"
+	Exec { &$uploader "$currentUploadCategory" "$version" $file "$log" }
 	
 	if ($lastExitCode -ne 0) {
 		Write-Host "Failed to upload to S3: $lastExitCode. UploadTryCount: $uploadTryCount"
