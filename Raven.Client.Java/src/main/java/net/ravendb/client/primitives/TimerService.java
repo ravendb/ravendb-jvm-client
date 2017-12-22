523e30249791a384417afe2ca587af827edcc078
Marcin Lewandowski <marcin@ravendb.net>
2017-12-22 14:32:37 +0100

    timer service - create threads as deamons

--- Raven.Client.Java/src/main/java/net/ravendb/client/primitives/TimerService.java
+++ Raven.Client.Java/src/main/java/net/ravendb/client/primitives/TimerService.java
@@ -4,5 +4,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class TimerService {
-    public static final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+    public static final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor(runnable -> {
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        return thread;
+    });
 }
