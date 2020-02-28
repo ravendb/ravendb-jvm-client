157d6fe0b21db3c5e71e34ebc1f0433821a40733
Marcin Lewandowski <marcin@ravendb.net>
2020-02-28 20:16:36 +0100

    sync with 4.2: 36b90f1e6f372f44d5d1bac04d0e0c416d341315 - @refresh feature

---
+++ src/main/java/net/ravendb/client/documents/operations/refresh/ConfigureRefreshOperation.java
@@ -0,0 +1,74 @@
+package net.ravendb.client.documents.operations.refresh;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import net.ravendb.client.documents.conventions.DocumentConventions;
+import net.ravendb.client.documents.operations.IMaintenanceOperation;
+import net.ravendb.client.http.IRaftCommand;
+import net.ravendb.client.http.RavenCommand;
+import net.ravendb.client.http.ServerNode;
+import net.ravendb.client.json.ContentProviderHttpEntity;
+import net.ravendb.client.primitives.Reference;
+import net.ravendb.client.util.RaftIdGenerator;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ContentType;
+
+import java.io.IOException;
+
+public class ConfigureRefreshOperation implements IMaintenanceOperation<ConfigureRefreshOperationResult> {
+
+    private final RefreshConfiguration _configuration;
+
+    public ConfigureRefreshOperation(RefreshConfiguration configuration) {
+        _configuration = configuration;
+    }
+
+    @Override
+    public RavenCommand<ConfigureRefreshOperationResult> getCommand(DocumentConventions conventions) {
+        return new ConfigureRefreshCommand(_configuration);
+    }
+
+    private static class ConfigureRefreshCommand extends RavenCommand<ConfigureRefreshOperationResult> implements IRaftCommand {
+        private final RefreshConfiguration _configuration;
+
+        public ConfigureRefreshCommand(RefreshConfiguration configuration) {
+            super(ConfigureRefreshOperationResult.class);
+            _configuration = configuration;
+        }
+
+        @Override
+        public boolean isReadRequest() {
+            return false;
+        }
+
+        @Override
+        public HttpRequestBase createRequest(ServerNode node, Reference<String> url) {
+            url.value = node.getUrl() + "/databases/" + node.getDatabase() + "/admin/refresh/config";
+
+            HttpPost request = new HttpPost();
+            request.setEntity(new ContentProviderHttpEntity(outputStream -> {
+                try (JsonGenerator generator = mapper.getFactory().createGenerator(outputStream)) {
+                    generator.getCodec().writeValue(generator, _configuration);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }, ContentType.APPLICATION_JSON));
+
+            return request;
+        }
+
+        @Override
+        public void setResponse(String response, boolean fromCache) throws IOException {
+            if (response == null) {
+                throwInvalidResponse();
+            }
+
+            result = mapper.readValue(response, ConfigureRefreshOperationResult.class);
+        }
+
+        @Override
+        public String getRaftUniqueRequestId() {
+            return RaftIdGenerator.newId();
+        }
+    }
+}
---
+++ src/test/java/net/ravendb/client/test/issues/RavenDB_13735Test.java
@@ -0,0 +1,73 @@
+package net.ravendb.client.test.issues;
+
+import com.google.common.base.Stopwatch;
+import net.ravendb.client.RemoteTestBase;
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.IDocumentStore;
+import net.ravendb.client.documents.operations.refresh.ConfigureRefreshOperation;
+import net.ravendb.client.documents.operations.refresh.ConfigureRefreshOperationResult;
+import net.ravendb.client.documents.operations.refresh.RefreshConfiguration;
+import net.ravendb.client.documents.session.IDocumentSession;
+import net.ravendb.client.exceptions.TimeoutException;
+import net.ravendb.client.infrastructure.entities.User;
+import net.ravendb.client.primitives.NetISO8601Utils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RavenDB_13735Test extends RemoteTestBase {
+
+    private void setupRefresh(IDocumentStore store) {
+        RefreshConfiguration config = new RefreshConfiguration();
+        config.setDisabled(false);
+        config.setRefreshFrequencyInSec(1L);
+
+        store.maintenance().send(new ConfigureRefreshOperation(config));
+    }
+
+    @Test
+    public void refreshWillUpdateDocumentChangeVector() throws Exception {
+        try (IDocumentStore store = getDocumentStore()) {
+            setupRefresh(store);
+
+            String expectedChangeVector = null;
+            try (IDocumentSession session = store.openSession()) {
+                User user = new User();
+                user.setName("Oren");
+                session.store(user, "users/1-A");
+
+                Date hourAgo = DateUtils.addHours(new Date(), -1);
+                session.advanced().getMetadataFor(user).put("@refresh", NetISO8601Utils.format(hourAgo));
+
+                session.saveChanges();
+
+                expectedChangeVector = session.advanced().getChangeVectorFor(user);
+            }
+
+            Stopwatch sw = Stopwatch.createStarted();
+
+            while (true) {
+                if (sw.elapsed(TimeUnit.SECONDS) > 10) {
+                    throw new TimeoutException();
+                }
+
+                try (IDocumentSession session = store.openSession()) {
+                    User user = session.load(User.class, "users/1-A");
+                    assertThat(user)
+                            .isNotNull();
+
+                    if (!session.advanced().getChangeVectorFor(user).equals(expectedChangeVector)) {
+                        // change vector was changed - great!
+                        break;
+                    }
+                }
+
+                Thread.sleep(200);
+            }
+        }
+    }
+}
