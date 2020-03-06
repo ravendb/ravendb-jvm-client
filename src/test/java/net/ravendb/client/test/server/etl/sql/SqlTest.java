5a60f6c5c65d1103b44ab9232840843ebd7933bf
Marcin Lewandowski <marcin@ravendb.net>
2020-03-06 15:22:43 +0100

    sync with .net: 60313b8c4c87c684c8c46141d6ad590595120850  - sql read test

--- src/test/java/net/ravendb/client/test/server/etl/sql/SqlTest.java
+++ src/test/java/net/ravendb/client/test/server/etl/sql/SqlTest.java
@@ -2,17 +2,22 @@ package net.ravendb.client.test.server.etl.sql;
 
 import net.ravendb.client.ReplicationTestBase;
 import net.ravendb.client.documents.IDocumentStore;
+import net.ravendb.client.documents.operations.GetOngoingTaskInfoOperation;
 import net.ravendb.client.documents.operations.connectionStrings.PutConnectionStringOperation;
 import net.ravendb.client.documents.operations.connectionStrings.PutConnectionStringResult;
 import net.ravendb.client.documents.operations.etl.*;
 import net.ravendb.client.documents.operations.etl.sql.SqlConnectionString;
 import net.ravendb.client.documents.operations.etl.sql.SqlEtlConfiguration;
 import net.ravendb.client.documents.operations.etl.sql.SqlEtlTable;
+import net.ravendb.client.documents.operations.ongoingTasks.OngoingTaskSqlEtlDetails;
+import net.ravendb.client.documents.operations.ongoingTasks.OngoingTaskState;
+import net.ravendb.client.documents.operations.ongoingTasks.OngoingTaskType;
 import net.ravendb.client.documents.session.IDocumentSession;
 import net.ravendb.client.infrastructure.entities.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,6 +59,37 @@ public class SqlTest extends ReplicationTestBase {
 
             assertThat(etlResult.getTaskId())
                     .isPositive();
+
+            // and try to read ongoing sql task
+
+            OngoingTaskSqlEtlDetails ongoingTask = (OngoingTaskSqlEtlDetails) src.maintenance()
+                    .send(new GetOngoingTaskInfoOperation(etlResult.getTaskId(), OngoingTaskType.SQL_ETL));
+
+            assertThat(ongoingTask)
+                    .isNotNull();
+
+            assertThat(ongoingTask.getTaskId())
+                    .isEqualTo(etlResult.getTaskId());
+            assertThat(ongoingTask.getTaskType())
+                    .isEqualTo(OngoingTaskType.SQL_ETL);
+            assertThat(ongoingTask.getResponsibleNode())
+                    .isNotNull();
+            assertThat(ongoingTask.getTaskState())
+                    .isEqualTo(OngoingTaskState.ENABLED);
+            assertThat(ongoingTask.getTaskName())
+                    .isEqualTo("etlToDst");
+
+            SqlEtlConfiguration configuration = ongoingTask.getConfiguration();
+            List<Transformation> transforms = configuration.getTransforms();
+            assertThat(transforms)
+                    .hasSize(1);
+            assertThat(transforms.get(0).isApplyToAllDocuments())
+                    .isTrue();
+
+            assertThat(configuration.getSqlTables())
+                    .hasSize(1);
+            assertThat(configuration.getSqlTables().get(0).getTableName())
+                    .isEqualTo("Users");
         }
     }
 
