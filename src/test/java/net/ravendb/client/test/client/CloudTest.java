4fcd9d704f3b5ad22e148e36bee9e9d313b46db7
Marcin Lewandowski <marcin@ravendb.net>
2020-11-27 07:09:06 +0100

    RDBC-408 Create helper method to create keystore from crt and key file

--- src/test/java/net/ravendb/client/test/client/CloudTest.java
+++ src/test/java/net/ravendb/client/test/client/CloudTest.java
@@ -7,10 +7,6 @@ import net.ravendb.client.util.CertificateUtils;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.cert.Certificate;
-
 public class CloudTest {
 
     @Test
@@ -20,15 +16,8 @@ public class CloudTest {
 
             //String path = "C:\\temp\\free.marcintest.client.certificate\\PEM\\free.marcintest.client.certificate.pem";
             String path = "C:\\temp\\free.marcintest.client.certificate\\PEM\\pkcs8-pair.txt";
-            Certificate certificate = CertificateUtils.readCertificate(path);
-            PrivateKey privateKey = CertificateUtils.readPrivateKey(path);
 
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            keyStore.load(null, null);
-
-            keyStore.setKeyEntry("a", privateKey, "".toCharArray(), new Certificate[] { certificate });
-
-            store.setCertificate(keyStore);
+            store.setCertificate(CertificateUtils.createKeystore(path));
             store.initialize();
 
             try (IDocumentSession session = store.openSession()) {
