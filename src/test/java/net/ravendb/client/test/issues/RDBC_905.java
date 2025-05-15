c9fa7149113c6cacdaf64e8a9c658c6b50c5d9d9
LielNagar <liel.nagar@ravendb.net>
2025-05-15 11:18:12 +0300

    RDBC-906: Disable macOS WaitForUserContinueTest test

--- src/test/java/net/ravendb/client/test/issues/RDBC_905.java
+++ src/test/java/net/ravendb/client/test/issues/RDBC_905.java
@@ -3,9 +3,11 @@ package net.ravendb.client.test.issues;
 import net.ravendb.client.RemoteTestBase;
 import net.ravendb.client.documents.IDocumentStore;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 public class RDBC_905 extends RemoteTestBase {
 
+    @Disabled("Skipping test")
     @Test
     public void canOpenBrowserOnMacOs() throws Exception {
         try (IDocumentStore store = getDocumentStore()) {
