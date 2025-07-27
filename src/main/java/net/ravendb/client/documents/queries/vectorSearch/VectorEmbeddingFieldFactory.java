ccdd8ded9fd575409b4ea8c5efb1a8d473cc53a5
Maya Zattelman <maya.zattelman@ravendb.net>
2025-07-27 17:14:25 +0300

    RDBC-921 Added Vector Search to Client API

---
+++ src/main/java/net/ravendb/client/documents/queries/vectorSearch/VectorEmbeddingFieldFactory.java
@@ -0,0 +1,36 @@
+package net.ravendb.client.documents.queries.vectorSearch;
+
+import net.ravendb.client.documents.session.IVectorFieldFactory;
+import net.ravendb.client.documents.queries.vectorSearch.fields.VectorEmbeddingField;
+import net.ravendb.client.documents.queries.vectorSearch.fields.VectorEmbeddingTextField;
+import net.ravendb.client.documents.queries.vectorSearch.fields.VectorField;
+import net.ravendb.client.documents.session.IVectorEmbeddingField;
+import net.ravendb.client.documents.session.IVectorEmbeddingTextField;
+import net.ravendb.client.documents.session.IVectorField;
+
+/**
+ * Factory for creating vector fields
+ * @param <T> The type of the field
+ */
+public class VectorEmbeddingFieldFactory<T> implements IVectorFieldFactory<T> {
+
+    @Override
+    public IVectorEmbeddingTextField withText(T fieldName) {
+        return new VectorEmbeddingTextField<>(fieldName);
+    }
+
+    @Override
+    public IVectorEmbeddingField withEmbedding(T fieldName, VectorEmbeddingType storedEmbeddingQuantization) {
+        return new VectorEmbeddingField<>(fieldName, storedEmbeddingQuantization, false);
+    }
+
+    @Override
+    public IVectorEmbeddingField withBase64(T fieldName, VectorEmbeddingType storedEmbeddingQuantization) {
+        return new VectorEmbeddingField<>(fieldName, storedEmbeddingQuantization, true);
+    }
+
+    @Override
+    public IVectorField withField(T fieldName) {
+        return new VectorField<>(fieldName);
+    }
+}
