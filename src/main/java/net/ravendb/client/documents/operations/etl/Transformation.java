5091af64aadbcca461d9895dbab027da363e304a
Liel Nagar <liel.nagar@ravendb.net>
2026-01-28 15:43:48 +0200

    RDBC-987: removed counters and timeseries from transformation

--- src/main/java/net/ravendb/client/documents/operations/etl/Transformation.java
+++ src/main/java/net/ravendb/client/documents/operations/etl/Transformation.java
@@ -2,7 +2,6 @@ package net.ravendb.client.documents.operations.etl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Transformation {
     private String name;
@@ -12,8 +11,6 @@ public class Transformation {
     private String script;
 
     public Transformation() {
-        this.counters = new CountersTransformation(this);
-        this.timeSeries = new TimeSeriesTransformation(this);
     }
 
     private String documentIdPostfix;
@@ -64,68 +61,5 @@ public class Transformation {
 
     public void setDocumentIdPostfix(String documentIdPostfix) {
         this.documentIdPostfix = documentIdPostfix;
-    }
-
-    final CountersTransformation counters;
-
-    public CountersTransformation getCounters() { return counters; }
-
-    final class CountersTransformation {
-
-        private final Transformation parent;
-        boolean isAddingCounters;
-
-        public CountersTransformation(Transformation parent) {
-            this.parent = parent;
-        }
-
-        Map<String, String> collectionToLoadBehaviorFunction;
-
-        public boolean isAddingCounters() {
-            return isAddingCounters;
-        }
-
-        private void setAddingCounters(boolean value) {
-            this.isAddingCounters = value;
-        }
-
-        public Map<String, String> getCollectionToLoadBehaviorFunction() {
-            return collectionToLoadBehaviorFunction;
-        }
-
-        private void setCollectionToLoadBehaviorFunction(Map<String, String> value) {
-            this.collectionToLoadBehaviorFunction = value;
-        }
-    }
-
-    final TimeSeriesTransformation timeSeries;
-
-    public TimeSeriesTransformation getTimeSeries() { return timeSeries; }
-
-    static final class TimeSeriesTransformation {
-
-        private final Transformation parent;
-        boolean isAddingTimeSeries;
-        Map<String, String> collectionToLoadBehaviorFunction;
-
-        public boolean isAddingTimeSeries() {
-            return isAddingTimeSeries;
-        }
-
-        private void setAddingTimeSeries(boolean value) {
-            this.isAddingTimeSeries = value;
-        }
-
-        public Map<String, String> getCollectionToLoadBehaviorFunction() {
-            return collectionToLoadBehaviorFunction;
-        }
-
-        private void setCollectionToLoadBehaviorFunction(Map<String, String> map) {
-            this.collectionToLoadBehaviorFunction = map;
-        }
-
-        public TimeSeriesTransformation(Transformation parent) {
-            this.parent = parent;
-        }
     }
 }
