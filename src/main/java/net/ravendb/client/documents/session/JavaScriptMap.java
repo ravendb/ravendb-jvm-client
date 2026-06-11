3421e9246da877746df2875202b98c2496ae55e6
Liel Nagar <liel.nagar@ravendb.net>
2026-06-11 13:21:43 +0300

    RDBC-1064: patch dictionary keys using bracket notation
    JavaScriptMap put/remove now emit this.path["key"] instead of this.path.key,
    using a JSON string literal for the key, so patches work for keys containing
    special characters or non-identifier characters.
    
    Ported from ravendb release/v7.2 (41d79c8)

--- src/main/java/net/ravendb/client/documents/session/JavaScriptMap.java
+++ src/main/java/net/ravendb/client/documents/session/JavaScriptMap.java
@@ -1,5 +1,8 @@
 package net.ravendb.client.documents.session;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import net.ravendb.client.extensions.JsonExtensions;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,18 +26,30 @@ public class JavaScriptMap<TKey, TValue> {
     public JavaScriptMap<TKey, TValue> put(TKey key, TValue value) {
         String argumentName = getNextArgumentName();
 
-        scriptLines.add("this." + pathToMap + "." + key + " = args." + argumentName + ";");
+        scriptLines.add("this." + pathToMap + "[" + formatKeyForJavaScript(key) + "] = args." + argumentName + ";");
         parameters.put(argumentName, value);
         return this;
     }
 
     public JavaScriptMap<TKey, TValue> remove(TKey key) {
-        scriptLines.add("delete this." + pathToMap + "." + key + ";");
+        scriptLines.add("delete this." + pathToMap + "[" + formatKeyForJavaScript(key) + "];");
         return this;
     }
 
     private String getNextArgumentName() {
         return "val_" + argCounter++ + "_" + suffix;
+    }
+
+    private static String formatKeyForJavaScript(Object key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Dictionary key cannot be null");
+        }
+
+        try {
+            return JsonExtensions.getDefaultMapper().writeValueAsString(key.toString());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to format dictionary key for JavaScript", e);
+        }
     }
 
     String getScript() {
