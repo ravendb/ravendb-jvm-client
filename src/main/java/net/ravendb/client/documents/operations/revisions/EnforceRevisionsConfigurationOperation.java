98b46c91b93ed0f10b6d32ab425385071db85554
Liel Nagar <liel.nagar@ravendb.net>
2026-06-11 13:21:43 +0300

    RDBC-1064: add MaxOpsPerSecond to EnforceRevisionsConfigurationOperation
    Add an optional MaxOpsPerSecond throttle to the operation parameters, rejecting
    non-positive values, to limit documents processed per second on large datasets.
    
    Ported from ravendb release/v7.2 (41d79c8)

--- src/main/java/net/ravendb/client/documents/operations/revisions/EnforceRevisionsConfigurationOperation.java
+++ src/main/java/net/ravendb/client/documents/operations/revisions/EnforceRevisionsConfigurationOperation.java
@@ -28,6 +28,7 @@ public class EnforceRevisionsConfigurationOperation implements IOperation<Operat
          */
         private boolean includeForceCreated;
         private String[] collections;
+        private Integer maxOpsPerSecond;
 
         public boolean isIncludeForceCreated() {
             return includeForceCreated;
@@ -43,6 +44,23 @@ public class EnforceRevisionsConfigurationOperation implements IOperation<Operat
 
         public void setCollections(String[] collections) {
             this.collections = collections;
+        }
+
+        /**
+         * Limits the number of documents processed per second.
+         * Use this to throttle the operation and reduce resource consumption on large datasets.
+         * Default is {@code null} (no throttling).
+         * @return Maximum operations per second
+         */
+        public Integer getMaxOpsPerSecond() {
+            return maxOpsPerSecond;
+        }
+
+        public void setMaxOpsPerSecond(Integer maxOpsPerSecond) {
+            if (maxOpsPerSecond != null && maxOpsPerSecond <= 0) {
+                throw new IllegalStateException("MaxOpsPerSecond must be greater than 0");
+            }
+            this.maxOpsPerSecond = maxOpsPerSecond;
         }
     }
 
