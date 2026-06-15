15c56f00f91b0fc28b2afed13f65412625d2c997
Liel Nagar <liel.nagar@ravendb.net>
2026-06-15 14:47:57 +0300

    RDBC-1060: prepare for release

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 <dependency>
   <groupId>net.ravendb</groupId>
   <artifactId>ravendb</artifactId>
-  <version>7.2.0</version>
+  <version>7.2.2</version>
 </dependency>
 ```
 
 
 ### Gradle
 ```
-implementation 'net.ravendb:ravendb:7.2.0'
+implementation 'net.ravendb:ravendb:7.2.2'
 ```
 
 ## Documentation
--- src/main/java/net/ravendb/client/http/RequestExecutor.java
+++ src/main/java/net/ravendb/client/http/RequestExecutor.java
@@ -75,7 +75,7 @@ public class RequestExecutor implements CleanCloseable {
      */
     public static Consumer<HttpUriRequestBase> requestPostProcessor = null;
 
-    public static final String CLIENT_VERSION = "7.2.0";
+    public static final String CLIENT_VERSION = "7.2.2";
 
     private static final ConcurrentMap<HttpClientCacheKey, CloseableHttpClient> globalHttpClientCache = new ConcurrentHashMap<>();
 
